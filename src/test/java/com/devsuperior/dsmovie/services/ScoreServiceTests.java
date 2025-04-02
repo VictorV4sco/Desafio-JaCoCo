b6d781618615080bad61e62ee22b2dfdd01629fc
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-04-02 12:03:42 -0300

    User tests

--- src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
@@ -1,15 +1,48 @@
 package com.devsuperior.dsmovie.services;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.ScoreEntity;
+import com.devsuperior.dsmovie.entities.ScoreEntityPK;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.tests.ScoreFactory;
 
 @ExtendWith(SpringExtension.class)
 public class ScoreServiceTests {
 	
 	@InjectMocks
 	private ScoreService service;
+	
+	@Mock
+	private ScoreRepository repository;
+
+	private Long nonExistingId, existingId;
+	private ScoreDTO scoreDTO;
+	private ScoreEntity score;
+	
+	@BeforeEach
+	void setUp() {
+		
+		existingId = 1L;
+		nonExistingId = 100000000L;
+		
+		score = ScoreFactory.createScoreEntity();
+		scoreDTO = ScoreFactory.createScoreDTO();
+		
+		Mockito.when(repository.save(any())).thenReturn(score);
+
+	}
 	
 	@Test
 	public void saveScoreShouldReturnMovieDTO() {
