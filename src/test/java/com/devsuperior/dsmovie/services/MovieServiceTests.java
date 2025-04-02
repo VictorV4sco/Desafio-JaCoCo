9fb78551cf51a7e016b010b5af1aba82f38c0fe3
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-04-01 21:57:34 -0300

    Inset and Update Tests

--- src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -1,15 +1,51 @@
 package com.devsuperior.dsmovie.services;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class MovieServiceTests {
 	
 	@InjectMocks
 	private MovieService service;
+	
+	@Mock
+	private MovieRepository repository;
+	
+	private Long existingId, nonExistingId;
+	private MovieEntity movie;
+	private MovieDTO movieDTO;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		
+		existingId = 1L; 
+		nonExistingId = 100000000L;
+		
+		movie = new MovieEntity(1L, "Title", 777.77, 1, "Image");
+		movieDTO = new MovieDTO(movie);
+		
+		Mockito.when(repository.save(any())).thenReturn(movie);
+		
+		Mockito.when(repository.getReferenceById(existingId)).thenReturn(movie);
+		Mockito.when(repository.getReferenceById(nonExistingId)).thenThrow(EntityNotFoundException.class);
+
+	}
 	
 	@Test
 	public void findAllShouldReturnPagedMovieDTO() {
@@ -25,14 +61,26 @@ public class MovieServiceTests {
 	
 	@Test
 	public void insertShouldReturnMovieDTO() {
+		MovieDTO result = service.insert(movieDTO);
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getTitle(), "Title");
 	}
 	
 	@Test
 	public void updateShouldReturnMovieDTOWhenIdExists() {
+		MovieDTO result = service.update(existingId, movieDTO);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getId(), existingId);
 	}
 	
 	@Test
 	public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			@SuppressWarnings("unused")
+			MovieDTO result = service.update(nonExistingId, movieDTO);
+		});
+		
 	}
 	
 	@Test
